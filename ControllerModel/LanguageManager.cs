50bee7ce3370cc1a641bb3d0e42e0d6ad0bda5a0
luanaleen <jmaelice1104@gmail.com>
2025-05-15 13:38:07 +0200

    Add LanguageManager controller and delete Enumeration and Language model

---
+++ ControllerModel/LanguageManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace LanguageManagers
+{
+    public static class LanguageManager
+    {
+        public static ResourceManager resManager = new ResourceManager("View.Resources.Lang", Assembly.GetExecutingAssembly());
+
+        public static void SetLanguage(string cultureCode)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureCode);
+        }
+
+        public static string Get(string key)
+        {
+            return resManager.GetString(key);
+        }
+    }
+}
--- View/Program.cs
+++ View/Program.cs
@@ -1,26 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-using Enumerations;
-using Languages;
+using ControllerModel;
+using LanguageManagers;
 
 class Program
 {
     static void Main()
     {
-        Language myLang = new Language();
-        Console.WriteLine("Langue actuelle : " + myLang.language);
-        Console.WriteLine("Veuillez entrer la langue souhaitée (fr, en) : ");
-        string input = Console.ReadLine()?.Trim().ToLower();
-
-        if (Enum.TryParse(typeof(EnumLanguage), input, true, out var result))
-        {
-            myLang.changeLanguage((EnumLanguage)result);
-            Console.WriteLine($"Langue après changement : " + myLang.language);
-        }
-        else
-        {
-            Console.WriteLine("Langue non reconnue. Langue inchangée.");
-        }
+       
 
     }
 }
