a745990a01a8eb95c76d705f2541800ce8b22af9
bounty <mathiscucherat@gmail.com>
2025-05-19 17:46:29 +0200

    add Ressources and Config file

---
+++ ControllerModel/SaveConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerModel
+{
+    public class SaveConfig
+    {
+        public string _pathToLogDaily { get; set; }
+        public string _language { get; set; }
+        public string _pathTologStatus { get; set; }
+
+        public SaveConfig(string _pathTologDaily, string _pathTologStatus, string _language) 
+        { 
+            this._pathToLogDaily = _pathTologDaily;
+            this._pathTologStatus = _pathTologStatus;
+            this._language = _language;
+        }
+
+    }
+}
