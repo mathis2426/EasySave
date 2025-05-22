9ac0c9e317af09057aaa3e19ef87153722b7d189
luanaleen <jmaelice1104@gmail.com>
2025-05-22 14:20:07 +0200

    Create home page

---
+++ WPFApp/HomePage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Logique d'interaction pour Page1.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        private Frame _mainFrame;
+        public HomePage(Frame mainFrame)
+        {
+            InitializeComponent();
+            _mainFrame = mainFrame;
+        }
+
+        private void TaskList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonLeave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ButtonCreateJob_Click(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new CreateJob(_mainFrame));
+        }
+        private void ButtonDeleteJob_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ButtonManageJob_Click(object sender, RoutedEventArgs e)
+        {
+
+            _mainFrame.Navigate(new ManageJob());
+
+        }
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
