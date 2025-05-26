7692b06c2fa2c007ec69070f130ecb12bf6f0ee4
aymerick <aymerick.boussot@gmail.com>
2025-05-26 10:33:16 +0200

    modification de commentaires

--- WPFApp/ViewModelManageJob.cs
+++ WPFApp/ViewModelManageJob.cs
@@ -33,10 +33,10 @@ namespace WPFApp
         public ICommand ResumeCommand { get; }
 
 
-        public ViewModelManageJob(JobObj Job) // constructeur 
+        public ViewModelManageJob(JobObj Job) // constructor
         {
             _job = Job;
-            StartCommand = new CommandHandler(() => StartJob(), CanStart); // Elle est liée à la méthode ConvertToUpper(), mais seulement si CanConvert() retourne true (exemple : input non vide).
+            StartCommand = new CommandHandler(() => StartJob(), CanStart);
             StopCommand = new CommandHandler(() => StopJob(), CanStop);
             ResumeCommand = new CommandHandler(() => ResumeJob(), CanResume);
             _progressValue = 72.8;
@@ -122,25 +122,25 @@ namespace WPFApp
                 OnPropertyChanged(nameof(JobID));
             }
         }
-        private void StartJob() //appel au model
+        private void StartJob() 
         {
             int result = Controller.LaunchBackup(JobID);
             OutputString = $"Demarrage du job {result}";
         }
 
-        private void StopJob() //appel au model
+        private void StopJob() 
         {
             int result = Controller.LaunchBackup(JobID);/////////////: à faiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiire
             OutputString = $"Demarrage du job {result}";
         }
 
-        private void ResumeJob() //appel au model
+        private void ResumeJob() 
         { 
             int result = Controller.LaunchBackup(JobID);/////////////: à faiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiire
             OutputString = $"Demarrage du job {result}";
         }
 
-        private bool CanStart() //valide si le bouton est cliquable, si le input est vide, le bouton est grisé
+        private bool CanStart() 
         {
 
             if (StateString != "Active" && StateString != "Stopped")
@@ -152,7 +152,7 @@ namespace WPFApp
                 return (false); 
             }
         }
-        private bool CanStop() //valide si le bouton est cliquable, si le input est vide, le bouton est grisé
+        private bool CanStop() 
         {
 
             if (StateString == "Active")
@@ -164,7 +164,7 @@ namespace WPFApp
                 return (false);
             }
         }
-        private bool CanResume() //valide si le bouton est cliquable, si le input est vide, le bouton est grisé
+        private bool CanResume() 
         {
 
             if (StateString == "Stopped")
@@ -177,7 +177,7 @@ namespace WPFApp
             }
         }
 
-        private void OnPropertyChanged([CallerMemberName] string? propertyName = null) // appelé quand une propriété change
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
