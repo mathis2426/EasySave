71ca467a080494e1ef3745f50d1494604abb1ef5
luanaleen <jmaelice1104@gmail.com>
2025-05-27 16:35:41 +0200

    Create ManageAllJob Page

---
+++ WPFApp/ManageAllJob.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Logique d'interaction pour ManageAllJob.xaml
+    /// </summary>
+    public partial class ManageAllJob : Page
+    {
+        public ManageAllJob()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLeave_ClickManageAllJob(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
